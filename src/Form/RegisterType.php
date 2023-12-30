9c316741750cc7539fa9ab59cd1d3ae4c952efdc
DMC <digitalmakingcompany@gmail.com>
2023-12-30 17:26:45 +0100

    create form about User and make some CSS

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,16 +4,78 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('lastname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Votre nom",
+                    'class' => 'text-center'
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 33
+                ])
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => "Votre prénom",
+                    'class' => 'text-center'
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 33
+            ])
+        ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 33,
+                ]),
+                'attr' => [
+                    'placeholder' => 'Votre adresse mail',
+                    'class' => 'text-center'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et sa confirmation doivent être identiques',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Votre mot de passe',
+                        'class' => 'text-center'
+                    ],
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => ['placeholder' => 'Confirmer',
+                    'class' => 'text-center'
+                    ],
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn btn-block btn-success text-light'
+                ]
+            ])
         ;
     }
 
